b9183cf719980daa6f51921eb775f8a4ce79356b
johnbdiving <97900594+johnbdiving@users.noreply.github.com>
2022-01-17 10:21:33 -0500

    Add files via upload

---
+++ ShapfileMergetoFC.py
@@ -0,0 +1,9 @@
+﻿import arcpy
+arcpy.env.workspace = arcpy.GetParameterAsText(0)
+arcpy.env.overwriteOutput = True
+InputFC = arcpy.GetParameterAsText(1)
+arcpy.AddMessage(InputFC)
+OutputFC = arcpy.GetParameterAsText(2)
+arcpy.management.Merge(InputFC, OutputFC)
+arcpy.AddMessage("\nScript Complete")
+
