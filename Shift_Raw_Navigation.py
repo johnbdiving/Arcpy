1b0e1e37f62c849a47b3b4f92ba8c32ddb9a2cf1
johnbdiving <97900594+johnbdiving@users.noreply.github.com>
2022-04-17 12:20:39 +0000

    Add files via upload
    Python script to shift the X and Y navigation values of an input table at user-specified amount

---
+++ Shift_Raw_Navigation.py
@@ -0,0 +1,42 @@
+﻿import os
+import pandas as pd
+import csv
+import glob
+
+## read csv file into dataframe
+rawFile=arcpy.GetParameterAsText(0)
+eastingShift=arcpy.GetParameterAsText(1)
+eastingField=arcpy.GetParameterAsText(2)
+northingShift=arcpy.GetParameterAsText(3)
+northingField=arcpy.GetParameterAsText(4)
+outputWorkspace=arcpy.GetParameterAsText(5)
+outputFileName=arcpy.GetParameterAsText(6)
+
+df=pd.read_csv(rawFile)
+arcpy.AddMessage(df.columns)
+arcpy.AddMessage(df.size)
+
+##run conversions to shift X and Y navigation 
+df['easting_shift']=float(eastingShift)
+df['X']=(df['{0}'.format(eastingField)]+df['easting_shift']).round(2)
+df['northing_shift']=float(northingShift)
+df['Y']=(df['{0}'.format(northingField)]+df['northing_shift']).round(2)
+
+arcpy.AddMessage(df.columns)
+arcpy.AddMessage(df.size)
+
+##select columns to remove prior to export into ouput table 
+del df['easting_shift']
+del df['northing_shift']
+del df['{0}'.format(eastingField)]
+del df['{0}'.format(northingField)]
+
+arcpy.AddMessage(df.columns)
+arcpy.AddMessage(df.size)
+
+## export result as csv file
+os.chdir(outputWorkspace)
+df.to_csv('{0}.txt'.format(outputFileName),index=False)
+
+arcpy.AddMessage("Navigation Shifting Completed")
+
